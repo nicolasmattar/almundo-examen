5ccdee97912054f8574e5a06a358fa5ae49a8d49
Nicolas Mattar <nicolasmattar@gmail.com>
2018-06-02 12:00:43 -0300

    Comentarios en EmployeeComparator

--- src/main/java/com/nicolasmattar/almundo/examen1/util/EmployeeComparator.java
+++ src/main/java/com/nicolasmattar/almundo/examen1/util/EmployeeComparator.java
@@ -4,6 +4,14 @@ import com.nicolasmattar.almundo.examen1.model.Employee;
 
 import java.util.Comparator;
 
+/**
+ * Compara un Empleado basado en Type.
+ * <p>
+ * Se respeta el siguiente orden:
+ * - OPERADOR
+ * - SUPERVISOR
+ * - DIRECTOR
+ */
 public class EmployeeComparator implements Comparator<Employee> {
 
     public int compare(Employee o1, Employee o2) {
