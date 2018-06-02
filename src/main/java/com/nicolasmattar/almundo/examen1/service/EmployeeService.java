0766473edd3ed186cf1faecfda2f6cf1fe1b1259
Nicolas Mattar <nicolasmattar@gmail.com>
2018-06-01 22:31:58 -0300

    EmployeeService

--- src/main/java/com/nicolasmattar/almundo/examen1/service/EmployeeService.java
+++ src/main/java/com/nicolasmattar/almundo/examen1/service/EmployeeService.java
@@ -6,11 +6,17 @@ import java.util.Comparator;
 import java.util.Queue;
 import java.util.concurrent.PriorityBlockingQueue;
 
+/**
+ * Servicio para administrar los Empleados.
+ */
 public class EmployeeService {
 
-    private Queue<Employee> employees;
+    private final Queue<Employee> employees;
 
     public EmployeeService(Comparator<? super Employee> comparator) {
+        if (comparator == null) {
+            throw new IllegalArgumentException("'comparator' no debe ser null.");
+        }
         this.employees = new PriorityBlockingQueue<>(11, comparator);
     }
 
@@ -18,19 +24,28 @@ public class EmployeeService {
         return employees.add(employee);
     }
 
-    public boolean removeEmployee(Object o) {
-        return employees.remove(o);
+    public boolean removeEmployee(Employee employee) {
+        return employees.remove(employee);
     }
 
     public void removeAllEmployees() {
         employees.clear();
     }
 
-
+    /**
+     * Devuelve el proximo empleado libre o espera a que haya uno disponible.
+     *
+     * @return siguiente empleado libre.
+     */
     public Employee getNextFreeEmployee() {
         return employees.poll();
     }
 
+    /**
+     * Libera a un empleado para que pueda atender otra llamada.
+     *
+     * @param employee
+     */
     public void freeEmployee(Employee employee) {
         employees.add(employee);
     }
